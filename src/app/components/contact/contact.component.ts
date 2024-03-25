6cccea289c5bf566f3da55325902f03a019de14c
Christophe Thibert <c.thibert89@gmail.com>
2024-03-25 23:08:40 +0100

    Validation formulaire

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,5 +1,10 @@
 import { Component, inject } from '@angular/core';
-import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormGroup,
+  FormControl,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 
 @Component({
@@ -13,17 +18,32 @@ export class ContactComponent {
   toastr: ToastrService = inject(ToastrService);
 
   contactForm = new FormGroup({
-    firstName: new FormControl(''),
-    lastName: new FormControl(''),
-    email: new FormControl(''),
+    firstName: new FormControl('', [Validators.required]),
+    lastName: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
   });
 
-  submitContact() {
-    const firstName: string = this.contactForm.value.firstName ?? '';
-    const lastName: string = this.contactForm.value.lastName ?? '';
-    const email: string = this.contactForm.value.email ?? '';
+  getFormValidationErrors() {
+    Object.keys(this.contactForm.controls).forEach((key) => {
+      const controlErrors = this.contactForm.get(key)?.errors;
+      if (controlErrors != null) {
+        Object.keys(controlErrors).forEach((keyError) => {
+          console.log('Key control: ' + key + ', keyError: ' + keyError);
+        });
+      }
+    });
+  }
 
-    console.log(firstName + ' ' + lastName + ' ' + email);
-    this.toastr.success('Envoyé !');
+  submitContact() {
+    if (this.contactForm.valid) {
+      const firstName: string = this.contactForm.value.firstName ?? '';
+      const lastName: string = this.contactForm.value.lastName ?? '';
+      const email: string = this.contactForm.value.email ?? '';
+
+      console.log(firstName + ' ' + lastName + ' ' + email);
+      this.toastr.success('Envoyé !');
+    } else {
+      this.getFormValidationErrors();
+    }
   }
 }
